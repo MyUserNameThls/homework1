4d74c5ec17603bd33901c01dba59c1061e7d40c4
MyUserNameThls <kolianich2104@gmail.com>
2026-04-25 22:21:30 +0300

    Add files via upload

---
+++ Leader-classic-7/js/header.js
@@ -0,0 +1,37 @@
+// export default class HeaderFixed {
+// 	constructor(config) {
+// 		this.headerTypes = config;
+// 		this.header = document.querySelector(`.${this.headerTypes.HEADER}`);
+
+// 		if (!this.header) {
+// 			throw new Error("Header element is missing.");
+// 		}
+
+// 		this.initEvents();
+// 	}
+
+// 	initEvents() {
+// 		window.addEventListener("scroll", this.updateFixedClass.bind(this));
+// 	}
+
+// 	updateFixedClass() {
+// 		if (window.scrollY > 0) {
+// 			this.header.classList.add(this.headerTypes.HEADER_FIXED);
+// 		} else {
+// 			this.header.classList.remove(this.headerTypes.HEADER_FIXED);
+// 		}
+// 	}
+
+// 	removeFixedClass() {
+// 		this.header.classList.remove(this.headerTypes.HEADER_FIXED);
+// 	}
+// }
+
+const header = document.querySelector(".header");
+// const html = document.querySelector(".page")
+
+export const funcHeader = () => {
+	if (window.scrollY > 0) {
+		header.classList.toggle("header--fixed");
+	}
+};
---
+++ Leader-classic-7/js/index.js
@@ -0,0 +1,33 @@
+import { funcHeader } from "./header.js";
+import { open, close } from "./burger.js";
+import { productSlider } from "./sizes-slider.js";
+
+funcHeader();
+
+try {
+	const headerFixed = new HeaderFixed({
+		HEADER: "header",
+		HEADER_FIXED: "header--fixed",
+	});
+
+	// new BurgerMenu(
+	// 	{
+	// 		BURGER: "burger",
+	// 		BURGER_OPEN: "burger--open",
+	// 		HEADER_MENU: "header__menu",
+	// 		HEADER_MENU_OPEN: "header__menu--open",
+	// 		lABEL: {
+	// 			OPEN: "Открыть меню",
+	// 			CLOSE: "Закрыть меню",
+	// 		},
+	// 		PAGE_BODY: "page__body",
+	// 		PAGE_BODY_NO_SCROLL: "page__body--no-scroll",
+	// 		MENU_LINK: "menu__link",
+	// 		BREAKPOINT: 992,
+	// 	},
+	// 	headerFixed,
+	// );
+	productSlider();
+} catch (error) {
+	console.error(error);
+}
